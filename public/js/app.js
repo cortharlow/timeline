d8b9fe2348a01f145e6ba99d50b72a84ef79bf3b
napeters <nicole.asha.peters@gmail.com>
2015-11-22 17:10:03 -0500

    can successfully use google maps api to show a map, marker, and location info using your lat/lng

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+function geoFindMe(){
+  navigator.geolocation.getCurrentPosition(success, error, geo_options);
+
+  function success(position) {
+    let map;
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: position.coords.latitude, lng: position.coords.longitude},
+      zoom: 15
+    });
+    let marker = new google.maps.Marker({
+      position: {lat: position.coords.latitude, lng: position.coords.longitude},
+      map: map,
+      animation: google.maps.Animation.DROP,
+      // label: 'H',
+      icon: 'http://www124.lunapic.com/do-not-link-here-use-hosting-instead/144823781631432?5066421535'
+    });
+
+    let geocoder = new google.maps.Geocoder;
+    let infowindow = new google.maps.InfoWindow;
+    geocoder.geocode({'location': {lat: position.coords.latitude, lng: position.coords.longitude}}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        if (results[1]) {
+          infowindow.setContent(results[1].formatted_address);
+          infowindow.open(map, marker);
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    });
+
+
+  };
+
+  function error(error) {
+    alert("Unable to retrieve your location due to "+error.code + " : " + error.message);
+  };
+
+  var geo_options = {
+    enableHighAccuracy: true,
+    maximumAge : 30000,
+    timeout : 27000
+  };
+}
