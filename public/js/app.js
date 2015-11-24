30687a12b2f66c220f91dd13d5fa1c3ae775cf3d
Cort Harlow <cort.harlow@gmail.com>
2015-11-24 15:33:32 -0500

    Timeline CSS begun

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// document.onload(function(){
-//   navigator.geolocation.getCurrentPosition(reportPosition, error, geo_options);
-// });
-
 window.onload = function() {
   let navLogin = document.getElementsByClassName('nav-login');
   let navSignup = document.getElementsByClassName('nav-signup');
@@ -117,11 +113,11 @@ function reportPosition(position) {
 
 function generateGoogleMap(position) {
   let map;
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map-main'), {
     center: {lat: position.coords.latitude, lng: position.coords.longitude},
     zoom: 15,
     styles: [{"stylers": [
-      { hue: "#00ffe6" },
+      //{ hue: "#00ffe6" },
       { saturation: -20 },
       { lightness: -20 },
       { gamma: 1.51 }
@@ -130,9 +126,9 @@ function generateGoogleMap(position) {
   let marker = new google.maps.Marker({
     position: {lat: position.coords.latitude, lng: position.coords.longitude},
     map: map,
-    animation: google.maps.Animation.DROP,
+    animation: google.maps.Animation.DROP
     // label: 'H',
-    icon: 'http://www124.lunapic.com/do-not-link-here-use-hosting-instead/144823781631432?5066421535'
+    //icon: 'http://www124.lunapic.com/do-not-link-here-use-hosting-instead/144823781631432?5066421535'
   });
 
   let geocoder = new google.maps.Geocoder;
@@ -159,3 +155,6 @@ function geoFindMe(){
   };
   navigator.geolocation.getCurrentPosition(generateGoogleMap, error, geo_options);
 }
+
+var position = geoFindMe();
+generateGoogleMap(position);
